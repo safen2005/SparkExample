80c10140c8819fe4e3f0cb7bb16573c0faa9265f
yangshao2 <yangshao2@iflytek.com>
2017-05-12 15:17:10 +0800

    safen2005/yangshao2 修改完毕 20170512 星期五

--- src/main/scala/com/mobin/SparkRDDFun/TransFormation/KVRDD/Cogroup.scala
+++ src/main/scala/com/mobin/SparkRDDFun/TransFormation/KVRDD/Cogroup.scala
@@ -11,7 +11,7 @@ object Cogroup {
     val conf = new SparkConf().setMaster("local").setAppName("ReduceByKey")
     val sc = new SparkContext(conf)
     val arr = List(("A", 1), ("B", 2), ("A", 2), ("B", 3))
-    val arr1 = List(("A", "A1"), ("B", "B1"), ("A", "A2"), ("B", "B2"))
+    val arr1 = List(("A", "A1"), ("B", "B1"), ("A", "A2"), ("B", "B2"), ("B", "B3"))
     val rdd = sc.parallelize(arr, 3)
     val rdd1 = sc.parallelize(arr1, 3)
     val groupByKeyRDD = rdd.cogroup(rdd1)
